bf2c21b1ae70e1ba6559d175e9fd63385ba9685a
Kelsi Riley <kriley@caltech.edu>
2018-11-05 13:37:21 -0800

    added some additional necessary fields to the link and router classes.

--- link.py
+++ link.py
@@ -1,12 +1,12 @@
 class Link:
-    def __init__(self, id, rate, delay, buffersize):
+    def __init__(self, id, connection1, connection2, rate, delay, buffersize):
         self.id = id
         self.rate = rate
         self.delay = delay
         self.buffersize = buffersize
+        self.buffer = []
+        self.connections = [connection1, connection2]
         # TODO:
-        #       - store the buffer
-        #       - store information about what the link is connecting
         #       - represent static cost of link.
 
     # TODO: implement packet sending functionality
--- router.py
+++ router.py
@@ -1,9 +1,10 @@
 class Router:
-    def __init__(self, id):
+    def __init__(self, id, links):
         self.id = id
+        self.links = links
         # TODO:
         #       - each router needs a routing table.
-
+        
     # TODO:
     #       - implement algorithm for routers to discover each other and
     #         communicate routing tables.
