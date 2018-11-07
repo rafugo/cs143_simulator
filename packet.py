e59fc7512585abc7e436144684c62037d3102446
Kelsi Riley <kriley@caltech.edu>
2018-11-07 11:01:57 -0800

    added outline of simulator class

--- packet.py
+++ packet.py
@@ -1,8 +1,9 @@
 class Packet:
     # TODO: figure out how to represent data, most notably being the routing
     #       tables which must be communicated between the routers.
-    def __init__(self, source, destination, numberinsequence, sequencesize, ackflag, data = ''):
+    def __init__(self, source, flow, destination, numberinsequence, sequencesize, ackflag, data = ''):
         self.source = source
+        self.flow = flow 
         self.destination = destination
         self.numberinsequence = numberinsequence
         self.sequencesize = sequencesize
---
+++ simulator.py
@@ -0,0 +1,10 @@
+class Simulator:
+    def __init__(self, filename):
+        # TODO:
+        #       - read contents of the file.
+
+    def run():
+    #TODO
+
+    def plot_metrics():
+    #TODO
