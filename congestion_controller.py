b69158640385c44e891de9dc87a1542686559846
Sakthi  Vetrivel <svetrivel@Sakthis-MacBook-Air.local>
2018-11-26 21:28:27 -0800

    simplifying congestion controller

--- congestion_controller.py
+++ congestion_controller.py
@@ -2,25 +2,21 @@ import sys
 
 class CongestionController:
     '''
-        ssthresh: Slow Start Threshold
-        cwnd: Congestion Window Size
-        timeout: Time period after which TCP times out
+        ssthresh: slow start threshold
+        cwnd: congestion window size
+        timeout: time period after which TCP times out
         not_acknowledged: Dictionary with IDs of unacknowledged packets as key,
             and timestamp that packet was sent as value
-        timed_out: List of packets whose acknowledgements haven't been received,
+        timed_out: list of packets whose acknowledgements haven't been received,
             and have now timed out
-        duplicate_count: Counter of the number of duplicate acknowledgements
-            we have received (to determine number of dropped packets)
+        retransmit: flag describing if packets have timed out and need to be
+            retransmitted
+        duplicate_count: the number of duplicate acknowledgements to
+            determine number of dropped packets
         last_ack_received: ID of the last acknowledgement packet received
-        window_start: ID of the first packet in the congestion window
-        retransmit: Boolean specifying whether packets have timed out and must
-            be retransmitted
-        flow: Flow that the Congestion controller belongs to
-        wake_event: Event specifying whether the controller should continue
-            sending packets
-
-        event_scheduler: Priority queue of events to hold FlowWakeEvents
-        clock: Clock for congestion controller
+        first_packet: ID of the first packet in the congestion window
+        flow_id: flow that the congestion controller belongs to
+        clock: clock for congestion controller
     '''
     def __init__(self):
         self.ssthresh = 50
@@ -28,14 +24,11 @@ class CongestionController:
         self.timeout = 1000
         self.not_acknowledged = dict()
         self.timed_out = []
+        self.retransmit = False
         self.duplicate_count = 0
         self.last_ack_received = -1
-        self.window_start = 0
-        self.retransmit = False
-        self.flow = None
-        self.wake_event = None
-
-        self.event_scheduler = None
+        self.first_packet = 0
+        self.flow_id = None
         self.clock = None
 
     # functions to use before congestion contoller method has actually been implemented
